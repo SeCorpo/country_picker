736397c8e379efbaf30fbd39ae6074f81a94ff16
semkoldewijn <sem@secorp.pro>
2025-07-25 23:26:31 +0200

    added exceptions

--- country_picker/gui/controllers/country_picker_controller.py
+++ country_picker/gui/controllers/country_picker_controller.py
@@ -1,6 +1,7 @@
 from PyQt6.QtCore import QObject, QThread, pyqtSignal
 import locale
 from country_picker.core.service.fetch_countries_raw import fetch_countries_raw
+from requests.exceptions import ConnectionError, Timeout, HTTPError
 
 class CountryFetchThread(QThread):
     """ Background thread to fetch and emit a locale-sorted (fixes Åland Islands from being last) list of country names """
@@ -14,8 +15,14 @@ class CountryFetchThread(QThread):
             names = [c["name"] for c in countries if isinstance(c, dict) and "name" in c]
             sorted_names = sorted(names, key=locale.strxfrm)
             self.countries_fetched.emit(sorted_names)
+        except ConnectionError:
+            self.fetch_failed.emit("Connection error")
+        except Timeout:
+            self.fetch_failed.emit("Timeout error")
+        except HTTPError as e:
+            self.fetch_failed.emit(f"HTTP error: {e.response.status_code}")
         except Exception as e:
-            self.fetch_failed.emit(str(e))
+            self.fetch_failed.emit(f"Unexpected error: {str(e)}")
 
 
 class CountryPickerController(QObject):
